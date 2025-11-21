f82c5219315bb93cb67781498e3d760143e380fc
Mukesh <mukeshdixena123@gmail.com>
2025-11-21 19:05:46 +0530

    chore: initial Spring Boot backend with all dependencies

---
+++ src/main/java/com/salon/backend_salon_managemen/BackendSalonManagemenApplication.java
@@ -0,0 +1,13 @@
+package com.salon.backend_salon_managemen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSalonManagemenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSalonManagemenApplication.class, args);
+	}
+
+}
